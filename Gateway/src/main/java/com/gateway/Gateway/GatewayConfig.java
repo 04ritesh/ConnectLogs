53349f6e36cd646c040a5be7be3fd786814b23bc
Ritesh Kumar <rockyhandsome1944@gmail.com>
2025-12-17 00:40:15 +0530

    Update Gateway routes to Railway URLs

--- Gateway/src/main/java/com/gateway/Gateway/GatewayConfig.java
+++ Gateway/src/main/java/com/gateway/Gateway/GatewayConfig.java
@@ -12,13 +12,13 @@ public class GatewayConfig {
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("user-service", r -> r.path("/api/users/**")
-                        .uri("http://localhost:8081"))
+                        .uri("https://connectlogs-production-31ac.up.railway.app"))
                 .route("experience-service", r -> r.path("/api/experiences/**")
-                        .uri("http://localhost:8082"))
+                        .uri("https://connectlogs-production-23c9.up.railway.app"))
                 .route("tag-service", r -> r.path("/api/tags/**")
                         .uri("http://localhost:8083"))
                 .route("likes-service", r -> r.path("/api/likes/**")
-                        .uri("http://localhost:8084"))
+                        .uri("https://connectlogs-production-640c.up.railway.app"))
                 .build();
     }
 }
